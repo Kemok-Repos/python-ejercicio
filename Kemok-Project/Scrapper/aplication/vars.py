ba1bab678359aa0cedb1fb4106f6b27ac95033b7
leo98ber <leonardoberoes94@gmail.com>
2022-11-27 18:41:51 -0400

    feat: add scrapper api
    BREAKING CHANGE: This import from other repository,because i had a problem with histories of the repositories

---
+++ Kemok-Project/Scrapper/aplication/vars.py
@@ -0,0 +1,5 @@
+URLS = {'base_url': 'http://localhost:8000/handle/handle'}
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
+SITE_URL = 'https://webscraper.io/test-sites/e-commerce/static/computers/laptops'
+
+
